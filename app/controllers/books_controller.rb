888c3cf9f74e5efebba5292137c40d4c6055b422
EC2 Default User <ec2-user@ip-172-31-37-142.ap-northeast-1.compute.internal>
2021-08-01 21:28:44 +0000

    [add]エラーメッセージの表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,10 +8,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    flash[:notice] = "successfully"
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+
   end
 
   def show
